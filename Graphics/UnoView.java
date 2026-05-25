1056609a62a7de3540cfc5c7fa4299b8d25d1108
chloelau26 <chloeyikshan.lau26@ycdsbk12.ca>
2026-05-25 14:05:18 -0400

    help screen & instruction screen paintcomponent alter

--- Graphics/UnoView.java
+++ Graphics/UnoView.java
@@ -8,9 +8,20 @@ import javax.imageio.ImageIO;
 
 public class UnoView extends JPanel implements ActionListener{
 	// Properties
-	// private UnoModel model; (have to connect with the model)
-	private JTextArea chatArea;
-	private JTextField chatInput;
+	int intWidth = 1280;
+	int intHeight = 720;
+	
+	// JFrame 
+	JFrame theFrame = new JFrame("UNO");
+	
+	// Color
+	Color transparentBlack = new Color(0, 0, 0, 200);
+	
+	// Font
+	Font titleFont = new Font("Georgia", Font.BOLD, 36);
+	Font headerFont = new Font("Georgia", Font.BOLD, 22);
+	Font bodyFont = new Font("Georgia", Font.PLAIN, 14);	
+
 	
 	// Screen
 	boolean blnHelp = false;
@@ -32,28 +43,79 @@ public class UnoView extends JPanel implements ActionListener{
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      
+		drawInstruction(g2);
+        /*
+        if(blnHelp){
+			drawHelp(g2);
+		}
+		*/
 	}
 	
+	// Draw Help
 	private void drawHelp(Graphics2D g2){
-	
+		g2.setColor(transparentBlack);
+		g2.fillRoundRect(200, 100, 880, 520, 10, 10);
+		
+		g2.setColor(Color.WHITE);
+		g2.setFont(titleFont);
+		g2.drawString("Help", 600, 150);
 	}
 	
 	private void drawInstruction(Graphics2D g2){
+		g2.setColor(transparentBlack);
+		g2.fillRoundRect(200, 100, 880, 520, 10, 10);
+		
+		g2.setColor(Color.WHITE);
+		g2.setFont(titleFont);
+		g2.drawString("Instruction", 550, 150);
 	}
 	
 	private void drawYourTurn(Graphics2D g2){}
-	private void drawWait(Graphics2D g2){}
+	
+	private void drawWait(Graphics2D g2){
+		/*g2.drawImage(
+		 * 
+		 * imgWait
+		 * imgWin
+		 * imgPickCard
+		*/
+	}
+	
 	private void drawTheme(Graphics2D g2){}
 	private void drawChat(Graphics2D g2){}
 	
 	// Constructor
 	public UnoView(){
-		// this.model = model;
-		this.setPreferredSize(new Dimension(1280, 720));
+		// Panel Setup
+		this.setLayout(null);
+		this.setPreferredSize(new Dimension(intWidth, intHeight));
+		
+		// Frame Setup
+		theFrame.setContentPane(this);
+		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theFrame.pack();
+		theFrame.setResizable(false);
+		theFrame.setVisible(true);
 		
 		// Timer (60 fps)
 		Timer timer = new Timer(16, this);
 		timer.start();
+		
+		// load images
+		/*
+		try{
+			imgStart = ImageIO.read(new File("start.png"));
+			imgBackground = ImageIO.read(new File("background.png"));
+		}catch(IOException e){
+			System.out.println("Unable to load image");
+		}
+		*/
+	}
+	
+	// Main Method
+	public static void main(String[] args){
+		new UnoView();
 	}
 	
 }
