961fe0edcd5a063e19dbe303640481f32bde7e14
chloelau26 <chloeyikshan.lau26@ycdsbk12.ca>
2026-06-09 12:51:40 -0400

    created javadoc

---
+++ javadoc/tag-search-index.js
@@ -0,0 +1,1 @@
+tagSearchIndex = [{"l":"Serialized Form","h":"","k":"18","u":"serialized-form.html"},{"l":"SuperSocketMaster","h":"class SuperSocketMaster","k":"16","u":"SuperSocketMaster.html#supersocketmaster-heading"}];updateSearchResults();
