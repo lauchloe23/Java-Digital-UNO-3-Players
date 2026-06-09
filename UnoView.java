ea5e48f3f13cca1427eab62db7f4598a34d9264c
chloelau26 <chloeyikshan.lau26@ycdsbk12.ca>
2026-06-08 21:32:17 -0400

    connection issue between windows and mac

--- UnoView.java
+++ UnoView.java
@@ -1772,31 +1772,27 @@ public class UnoView extends JPanel implements ActionListener, MouseListener, Ke
 
 	//added
 	private String getLocalIPAddress(){
-		String strBestIP = "127.0.0.1";
 		try{
-			java.util.Enumeration<java.net.NetworkInterface> interfaces = java.net.NetworkInterface.getNetworkInterfaces();
+			java.util.Enumeration<java.net.NetworkInterface> interfaces = 
+				java.net.NetworkInterface.getNetworkInterfaces();
 			while(interfaces.hasMoreElements()){
 				java.net.NetworkInterface iface = interfaces.nextElement();
-				if(iface.isLoopback() || !iface.isUp()){
-					continue;
-				}
+				if(iface.isLoopback() || !iface.isUp()) continue;
+				
 				java.util.Enumeration<java.net.InetAddress> addresses = iface.getInetAddresses();
 				while(addresses.hasMoreElements()){
 					java.net.InetAddress addr = addresses.nextElement();
 					if(addr instanceof java.net.Inet4Address && addr.isSiteLocalAddress()){
-						System.out.println("Found IP on [" + iface.getDisplayName() + "]: " + addr.getHostAddress());
-						String strName = iface.getDisplayName().toLowerCase();
-						if(strName.contains("wi-fi") || strName.contains("wifi") || strName.contains("wireless") || strName.contains("ethernet")){
-							return addr.getHostAddress();
-						}
-						strBestIP = addr.getHostAddress(); // fallback 
+						System.out.println("Using IP: " + addr.getHostAddress() 
+							+ " from interface: " + iface.getDisplayName());
+						return addr.getHostAddress();
 					}
 				}
 			}
 		}catch(Exception e){
 			System.out.println("Could not get local IP: " + e.getMessage());
 		}
-		return strBestIP;
+		return "127.0.0.1";
 	}
 	/*
 	private String getLocalIPAddress(){
